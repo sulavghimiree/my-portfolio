814c45fdeef6120a92333d444a1648abc39d7642
Sulav Ghimire <sulavg2003@gmail.com>
2024-05-25 21:29:53 +0545

    first commit

---
+++ utils/data/educations.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor Degree",
+    duration: "2021 - Present",
+    institution: "School of Engineering, Pokhara University",
+  },
+  {
+    id: 2,
+    title: "School Leaving Certificate",
+    duration: "2017 - 2020",
+    institution: "Aroma Secondary School",
+  },
+  {
+    id: 3,
+    title: "Secondary Education Examination Certificate",
+    duration: "2006 - 2016",
+    institution: "Shiksha Bikash Secondary School",
+  }
+]
---
+++ utils/data/personal-data.js
@@ -0,0 +1,14 @@
+export const personalData = {
+  name: "Sulav Ghimire",
+  profile: '/profile.png',
+  designation: "Web Developer",
+  description: "My name is Sulav Ghimire. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'sulavg2003@gmail.com',
+  phone: '+977 9815240913',
+  address: 'Ratnanagar - 12, Chitwan, Nepal',
+  github: 'https://github.com/sulavghimiree',
+  facebook: 'https://www.facebook.com/sulavghimiree/',
+  linkedIn: 'https://www.linkedin.com/in/sulav-ghimire-425606258?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+  twitter: 'https://x.com/SulavGhimiree',
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
